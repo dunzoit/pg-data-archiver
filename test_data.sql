8335a1ec00c6d1fc2003613fb60fc79c01321731
Hari Prashanth <krhari@dunzo.in>
2019-11-01 13:57:18 +0530

    Added some test data and cleaned up readme

---
+++ test_data.sql
@@ -0,0 +1,32 @@
+CREATE TABLE employee (
+   id       INT NOT NULL,
+   name     varchar(20),
+   dept     varchar(10),
+   salary   int,
+   PRIMARY KEY (id)
+);
+
+INSERT INTO employee VALUES (1,'Thomas','Sales',5000);
+INSERT INTO employee VALUES (2,'Jason','Technology',5500);
+INSERT INTO employee VALUES (3,'Mayla','Technology',7000);
+INSERT INTO employee VALUES (4,'Nisha','Marketing',9500);
+INSERT INTO employee VALUES (5,'Randy','Technology',6000);
+INSERT INTO employee VALUES (6,'Thomas','Sales',5000);
+INSERT INTO employee VALUES (7,'Jason','Technology',5500);
+INSERT INTO employee VALUES (8,'Mayla','Technology',7000);
+INSERT INTO employee VALUES (9,'Nisha','Marketing',9500);
+INSERT INTO employee VALUES (10,'Randy','Technology',6000);
+INSERT INTO employee VALUES (11,'Thomas','Sales',5000);
+INSERT INTO employee VALUES (12,'Jason','Technology',5500);
+INSERT INTO employee VALUES (13,'Mayla','Technology',7000);
+INSERT INTO employee VALUES (14,'Nisha','Marketing',9500);
+INSERT INTO employee VALUES (15,'Randy','Technology',6000);
+INSERT INTO employee VALUES (16,'Jason','Technology',5500);
+INSERT INTO employee VALUES (18,'Mayla','Technology',7000);
+INSERT INTO employee VALUES (19,'Nisha','Marketing',9500);
+INSERT INTO employee VALUES (17,'Randy','Technology',6000);
+INSERT INTO employee VALUES (20,'Thomas','Sales',5000);
+INSERT INTO employee VALUES (21,'Jason','Technology',5500);
+INSERT INTO employee VALUES (22,'Mayla','Technology',7000);
+INSERT INTO employee VALUES (23,'Nisha','Marketing',9500);
+INSERT INTO employee VALUES (24,'Randy','Technology',6000);
